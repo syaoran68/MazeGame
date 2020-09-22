b8adf9209aa41c98449af62ff696cb4f412b1114
Eddie Huang <edhuang04@gmail.com>
2020-09-21 21:08:26 -0700

    mazeGeneration
    maze setup

--- assets/cs-sketch.js
+++ assets/cs-sketch.js
@@ -1,78 +1,161 @@
-var cols, rows;
-var w = 20;
-var grid = [];
-//current cell
-var current;
+var maze;
 
-function setup() {
-    createCanvas(800,800);
-    cols = floor(width/w);
-    rows = floor(height/w);
-
-    for(var j = 0; j < rows; j++)
-    {
-        for(var i = 0; i < cols; i++)
-        {
-            var cell = new Cell(i,j);
-            grid.push(cell);
-        }
-    }
-    current = grid[0];
+function setup()
+{
+    // set the canvas height and width
+    createCanvas(800, 800);
+    //set the block size
+    var s = 20;
+    //generate the row and columns based on the canvas size and the block size
+	var rows = floor(height / s);
+	var cols = floor(width / s);
+    
+    //setting up the maze functionality
+	maze = new mazeConstruction(rows, cols, s);
+    maze.initialize();
+    //setting the framerate for refreshing (theres a limitation to p5 how fast it can refresh)
+	frameRate(1000);
 }
+
 
 function draw() {
-    background(51);
-    for (var i = 0; i < grid.length; i++)
-    {
-        grid[i].show();
-    }
-
-    current.visited = true;
+	background(51);
+	maze.constructOneStep();
+	maze.display();
 }
 
-function Cell(i, j) {
-    this.i = i;
-    this.j = j;
-    //top right bottom left
-    this.walls = [true, true, true, true];
-    this.visited = false;
 
-    this.show = function()
-    {
-        var x = this.i*w;
-        var y = this.j*w;
-        stroke(255);
+function mazeConstruction(rows, cols, s)
+{
+    //importing data for rows, columns, and square size
+	this.rows = rows;
+	this.cols = cols;
+    this.size = s;                        
+    //new array for the walls
+    this.walls = [];            
+    //function for joining the two subsets together.
+	this.uSet = new joinSets();           
+    this.wallIndex = 0;
+    //creating the walls
+	this.initialize = function(){
+        //generating all the row walls
+		for(var i = 0; i <= this.rows; i++){
+			for(var j = 0; j < this.cols; j++){
+				this.walls.push(new wall(i, j, i, j + 1, this.size));
+			}
+        }
+        //generating all the column walls
+		for(var j = 0; j <= this.cols; j++){
+			for(var i = 0; i < this.rows; i++){
+				this.walls.push(new wall(i, j, i + 1, j, this.size));
+			}
+		}
+		//randomization of the walls
+		this.walls.sort(function randomSort(a, b){return Math.random() > 0.5 ? - 1 : 1;});
+		for(var i = 0; i < this.rows; i++){
+			for(var j = 0; j < this.cols; j++){
+				this.uSet.uset.push(new cell(i, j, this.rows, this.cols));
+			}
+		}
+	}
+	//displaying walls
+	this.display = function(){
+		for(var i = 0; i < this.walls.length; i++){
+			this.walls[i].isHighlight(false);
+		}
+		this.walls[this.wallIndex].isHighlight(true);
+		for(var i = 0; i < this.walls.length; i++){
+			this.walls[i].show();
+		}
+	}
+	//
+	this.constructOneStep = function(){
+        var wall = this.walls[this.wallIndex];
+        //setting all the walls on the proper sides    
+		var isBorder = (wall.i1 == wall.i2 && (wall.i1 == 0 || wall.i1 == this.rows)) || (wall.j1 == wall.j2 && (wall.j1 == 0 || wall.j1 == this.cols));
+		if(isBorder == false){
+			var c1Index, c2Index;
+			if(wall.i1 == wall.i2){
+				c1Index = (wall.i1 - 1) * this.cols + wall.j1;
+				c2Index = wall.i1 * this.cols + wall.j1;
+			}else if(wall.j1 == wall.j2){
+				c1Index = wall.i1 * this.cols + wall.j1 - 1;
+				c2Index = wall.i1 * this.cols + wall.j1;
+            }
+            //hunting for the parent node of each set we're trying to combine
+			var p1 = this.uSet.findParent(c1Index);          
+			var p2 = this.uSet.findParent(c2Index);
+			if (p1 != p2){
+                //mark the walls broken and so they don't display
+                this.walls[this.wallIndex].breaked = true
+                //merge the two subsets together
+				this.uSet.unionTwo(c1Index, c2Index);        
+			}
+		}
+		this.wallIndex++;
+		if(this.wallIndex >= this.walls.length){
+			this.wallIndex = this.walls.length - 1;
+		}
+	}
+}
 
-        
-        
-
-        if(this.walls[0])
-        {
-            line(x    , y    , x + w,y    );
-        }
-        if(this.walls[1])
-        {
-            line(x + w, y    , x + w,y + w);
-        }
-        if(this.walls[2])
-        {
-            line(x + w, y + w, x    ,y + w);
-        }
-        if(this.walls[3])
-        {
-            line(x    , y + w, x    ,y    );
-        }
-        line(x    , y    , x + w,y    );
-        line(x + w, y    , x + w,y + w);
-        line(x + w, y + w, x    ,y + w);
-        line(x    , y + w, x    ,y    );
-        
-        if(this.visited)
-        {
-            fill(255,0, 255, 100);
-            rect(x,y,w,w);
-        }
+//joining the subsets
+function joinSets(){
+    this.uset = [];
+    // hunting for the parent subset
+	this.findParent= function(ind){
+        var pind = this.uset[ind].parent;
+        //if its already the parent set then just return the index
+		if(pind == ind){
+			return ind;
+		}
+		var res = this.findParent(pind);
+		this.uset[ind].parent = res;
+		return res;
     }
+    //joing one subset under the other
+	this.unionTwo = function(ind1, ind2){
+		var p1 = this.findParent(ind1);
+		var p2 = this.findParent(ind2);
+		this.uset[p1].parent = p2;
+	}
+}
 
-    
+// cell setup
+function cell(i, j, rows, cols){
+	this.i = i;      
+	this.j = j;      
+	this.rows = rows;
+	this.cols = cols;
+	this.parent = i * cols + j;
+	this.index = i * cols + j;
+}
+
+// wall setup
+function wall(i1, j1, i2, j2, s){
+	this.i1 = i1;
+	this.j1 = j1;
+	this.i2 = i2;
+	this.j2 = j2;
+	this.s = s;
+    this.breaked = false;
+    //highlighted wall being processed at the moment
+	this.ishigh = false;                  
+	this.isHighlight = function(ishigh){
+		this.ishigh = ishigh;
+    }
+    //drawing the walls
+	this.show = function(){
+		if(this.breaked == false){
+			if(this.ishigh){
+                
+				stroke(255, 0, 0);
+				strokeWeight(4);
+			}else{
+				stroke(255);
+				strokeWeight(2);
+			}
+			line(this.j1 * this.s, this.i1 * this.s, this.j2 * this.s, this.i2 * s);
+		}
+	}
 }
